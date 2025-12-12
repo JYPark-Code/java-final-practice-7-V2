cc4808d384b2b3127fca239e95e910492ce6744d
jypark <toboy93@gmail.com>
2025-12-12 10:47:35 +0900

    feat(MainFunction): 기능 선택 Enum, (리모컨 역할)

---
+++ src/main/java/attendance/domain/menu/MainFunction.java
@@ -0,0 +1,30 @@
+package attendance.domain.menu;
+
+import java.util.Arrays;
+
+public enum MainFunction {
+
+    CHECK_ATTENDANCE("1"),
+    EDIT_ATTENDANCE("2"),
+    SHOW_RECORDS("3"),
+    CHECK_EXPULSION_DANGER("4"),
+    QUIT("Q");
+
+    private final String command;
+
+    MainFunction(String command){
+        this.command = command;
+    }
+
+    public static MainFunction from(String command) {
+        return Arrays.stream(values())
+                .filter(function -> function.command.equals(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하셨습니다."));
+    }
+
+    public boolean isQuit(){
+        return this == QUIT;
+    }
+
+}
